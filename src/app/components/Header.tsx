29ca48635304a44dc4b60e61bab0b7bda32dc2c1
Corneliu <29034380+mishimomei@users.noreply.github.com>
2025-10-24 22:39:40 +0300

    added back ui elements: badge.tsx, card.tsx, input.tsx, separator.tsx, textarea.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/app/components/ui/button";
 import { Menu, X } from "lucide-react";
 
 export function Header() {
